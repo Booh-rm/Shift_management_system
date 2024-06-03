b93114ba901c681a463790798a5f65191dfc9ae8
Booh-rm <stivenatis@udenar.edu.co>
2024-06-03 17:57:59 -0500

    Actualización base de datos

--- Backend/DB/database.sql
+++ Backend/DB/database.sql
@@ -1,19 +1,19 @@
 
 USE `Shift_management_system`;
 
-CREATE TABLE `usuario` (
+CREATE TABLE `tb_usuario` (
     `cedula` INT PRIMARY KEY,
     `nombre` VARCHAR(255) NOT NULL,
     `apellido` VARCHAR(255) NOT NULL,
     `telefono` VARCHAR(20)
 );
 
-CREATE TABLE `tipo_consulta` (
+CREATE TABLE `tb_tipo_consulta` (
     `codigo` VARCHAR(2) PRIMARY KEY,
     `descripcion` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `funcionario` (
+CREATE TABLE `tb_funcionario` (
     `cedula` VARCHAR(50) PRIMARY KEY,
     -- `id_turno` VARCHAR(50),
     `id_dependencia` VARCHAR(2),
@@ -21,10 +21,10 @@ CREATE TABLE `funcionario` (
     `apellido` VARCHAR(255),
     `email` VARCHAR(255),
     `contrasena` VARCHAR(255),
-    FOREIGN KEY (`id_dependencia`) REFERENCES `tipo_consulta`(`codigo`)
+    FOREIGN KEY (`id_dependencia`) REFERENCES `tb_tipo_consulta`(`codigo`)
     -- FOREIGN KEY (`id_turno`) REFERENCES `turno`(`id_turno`)
 );
-CREATE TABLE `turno` (
+CREATE TABLE `tb_turno` (
     `id_turno` VARCHAR(50) PRIMARY KEY,
     `id_usuario` INT,
     `id_consulta` VARCHAR(50),
@@ -32,7 +32,7 @@ CREATE TABLE `turno` (
     `desc_turno` VARCHAR(255),
     `fecha` DATE,
     `hora` TIME,
-    FOREIGN KEY (`id_usuario`) REFERENCES `usuario`(`cedula`),
-    FOREIGN KEY (`id_consulta`) REFERENCES `tipo_consulta`(`codigo`),
-    FOREIGN KEY (`id_funcionario`) REFERENCES `funcionario`(`cedula`)
+    FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuario`(`cedula`),
+    FOREIGN KEY (`id_consulta`) REFERENCES `tb_tipo_consulta`(`codigo`),
+    FOREIGN KEY (`id_funcionario`) REFERENCES `tb_funcionario`(`cedula`)
 );
