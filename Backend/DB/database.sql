b133d52336c24b9196c9d489d223a0f26f27e102
Booh-rm <stivenatis@udenar.edu.co>
2024-06-03 17:51:39 -0500

    Actualización base de datos

--- Backend/DB/database.sql
+++ Backend/DB/database.sql
@@ -13,26 +13,26 @@ CREATE TABLE `tipo_consulta` (
     `descripcion` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `turno` (
-    `id_turno` VARCHAR(50) PRIMARY KEY,
-    `id_usuario` INT,
-    `id_consulta` VARCHAR(50),
-    `desc_turno` VARCHAR(255),
-    `fecha` DATE,
-    `hora` TIME,
-    FOREIGN KEY (`id_usuario`) REFERENCES `usuario`(`cedula`),
-    FOREIGN KEY (`id_consulta`) REFERENCES `tipo_consulta`(`codigo`)
-);
-
-
 CREATE TABLE `funcionario` (
     `cedula` VARCHAR(50) PRIMARY KEY,
-    `id_turno` VARCHAR(50),
+    -- `id_turno` VARCHAR(50),
     `id_dependencia` VARCHAR(2),
     `nombre` VARCHAR(255),
     `apellido` VARCHAR(255),
     `email` VARCHAR(255),
     `contrasena` VARCHAR(255),
-    FOREIGN KEY (`id_dependencia`) REFERENCES `tipo_consulta`(`codigo`),
-    FOREIGN KEY (`id_turno`) REFERENCES `turno`(`id_turno`)
+    FOREIGN KEY (`id_dependencia`) REFERENCES `tipo_consulta`(`codigo`)
+    -- FOREIGN KEY (`id_turno`) REFERENCES `turno`(`id_turno`)
+);
+CREATE TABLE `turno` (
+    `id_turno` VARCHAR(50) PRIMARY KEY,
+    `id_usuario` INT,
+    `id_consulta` VARCHAR(50),
+    `id_funcionario` VARCHAR(50),
+    `desc_turno` VARCHAR(255),
+    `fecha` DATE,
+    `hora` TIME,
+    FOREIGN KEY (`id_usuario`) REFERENCES `usuario`(`cedula`),
+    FOREIGN KEY (`id_consulta`) REFERENCES `tipo_consulta`(`codigo`),
+    FOREIGN KEY (`id_funcionario`) REFERENCES `funcionario`(`cedula`)
 );
