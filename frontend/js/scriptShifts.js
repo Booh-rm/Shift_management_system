77caa0ca1fac33202f1fef23c802e88f5d1de371
Booh-rm <stivenatis@udenar.edu.co>
2024-06-09 17:12:33 -0500

    Validación de datos ingresados por el usuario

--- frontend/js/scriptShifts.js
+++ frontend/js/scriptShifts.js
@@ -10,23 +10,31 @@ document.getElementById('addFieldsBtn').addEventListener('click', function () {
         method: 'POST',
         data: { dni: dni }, // Envía el DNI al servidor
         success: function (response) {
+ 
+            // Verifica si el DNI existe en la base de datos y la dependencia es válida
             if (response === 'existe') {
-                // Si el DNI existe en la base de datos, muestra la ventana modal
-                var modal = document.getElementById('turnoModal');
-                modal.style.display = "block";
+                // Obtenemos el índice del elemento seleccionado en el select
+                var selectedIndex = document.getElementById('dependencia').selectedIndex;
+
+                // Verificamos si se ha seleccionado el primer elemento del select (índice 0)
+                if (selectedIndex === 0) {
+                    // Si la dependencia no es válida, muestra una alerta
+                    alert('Por favor, seleccione una dependencia válida.');
+                    // Salir de la función para evitar que se muestren las opciones adicionales
+                    return;
+                } else {
+                    // Si el DNI existe en la base de datos y la dependencia es válida, muestra la ventana modal
+                    var modal = document.getElementById('turnoModal');
+                    modal.style.display = "block";
+                }
             } else {
-                // Si el DNI no existe en la base de datos y los campos no se han agregado, agregar los campos adicionales
+                // Si el DNI no existe en la base de datos, verifica si los campos adicionales ya se han agregado
                 if (!fieldsAdded) {
+                    // Si los campos adicionales no se han agregado, agrégalos
                     agregarCamposAdicionales();
                 } else {
-                    // Si los campos ya se han agregado, enviar el formulario y registrar el cliente
+                    // Si los campos adicionales ya se han agregado, envía el formulario y registra el cliente
                     registerClient();
-                    
-                    
-                    // Si los campos ya se han agregado, enviar el formulario y mostrar el modal
-                    //document.getElementById('turnoForm').submit();
-                    // var modal = document.getElementById('turnoModal');
-                    // modal.style.display = "block";
                 }
             }
         }
@@ -35,72 +43,70 @@ document.getElementById('addFieldsBtn').addEventListener('click', function () {
 
 function agregarCamposAdicionales() {
     // Crear el contenedor adicional
-     // Crear el contenedor adicional
-     var additionalFields = document.getElementById('additionalFields');
+    var additionalFields = document.getElementById('additionalFields');
 
-     // Crear el primer nuevo campo y su etiqueta
-     var label1 = document.createElement('label');
-     label1.setAttribute('for', 'name');
-     label1.textContent = 'Nombre';
- 
-     var name = document.createElement('input');
-     name.type = 'text';
-     name.id = 'name';
-     name.name = 'name';
-     name.placeholder = 'Tu nombre...';
-     name.required = true;
- 
-     var label2 = document.createElement('label');
-     label2.setAttribute('for', 'lastName');
-     label2.textContent = 'Apellido';
- 
-     var lastName = document.createElement('input');
-     lastName.type = 'text';
-     lastName.id = 'lastName';
-     lastName.name = 'lastName';
-     lastName.placeholder = 'Tu apellido...';
-     lastName.required = true;
- 
-     var label3 = document.createElement('label');
-     label3.setAttribute('for', 'phone');
-     label3.textContent = 'Teléfono';
- 
-     var phone = document.createElement('input');
-     phone.type = 'text';
-     phone.id = 'phone';
-     phone.name = 'phone';
-     phone.placeholder = 'Tu número de teléfono...';
-     phone.required = true;
+    // Crear el primer nuevo campo y su etiqueta
+    var label1 = document.createElement('label');
+    label1.setAttribute('for', 'name');
+    label1.textContent = 'Nombre';
 
-     var label4 = document.createElement('label');
-     label4.setAttribute('for', 'email');
-     label4.textContent = 'Correo electrónico';
+    var name = document.createElement('input');
+    name.type = 'text';
+    name.id = 'name';
+    name.name = 'name';
+    name.placeholder = 'Tu nombre...';
+    name.required = true;
 
-     var email = document.createElement('input');
-     email.type = 'email';
-     email.id = 'email';
-     email.name = 'email';
-     email.placeholder = 'Tu email...';
-     email.required = true;
- 
-     // Añadir las etiquetas y los campos al contenedor
-     additionalFields.appendChild(label1);
-     additionalFields.appendChild(name);
-     additionalFields.appendChild(document.createElement('br'));
-     additionalFields.appendChild(label2);
-     additionalFields.appendChild(lastName);
-     additionalFields.appendChild(document.createElement('br'));
-     additionalFields.appendChild(label3);
-     additionalFields.appendChild(phone);
-     additionalFields.appendChild(document.createElement('br'));
-     additionalFields.appendChild(label4);
-     additionalFields.appendChild(email);
-     additionalFields.appendChild(document.createElement('br'));
+    var label2 = document.createElement('label');
+    label2.setAttribute('for', 'lastName');
+    label2.textContent = 'Apellido';
+
+    var lastName = document.createElement('input');
+    lastName.type = 'text';
+    lastName.id = 'lastName';
+    lastName.name = 'lastName';
+    lastName.placeholder = 'Tu apellido...';
+    lastName.required = true;
+
+    var label3 = document.createElement('label');
+    label3.setAttribute('for', 'phone');
+    label3.textContent = 'Teléfono';
+
+    var phone = document.createElement('input');
+    phone.type = 'text';
+    phone.id = 'phone';
+    phone.name = 'phone';
+    phone.placeholder = 'Tu número de teléfono...';
+    phone.required = true;
+
+    var label4 = document.createElement('label');
+    label4.setAttribute('for', 'email');
+    label4.textContent = 'Correo electrónico';
+
+    var email = document.createElement('input');
+    email.type = 'email';
+    email.id = 'email';
+    email.name = 'email';
+    email.placeholder = 'Tu email...';
+    email.required = true;
+
+    // Añadir las etiquetas y los campos al contenedor
+    additionalFields.appendChild(label1);
+    additionalFields.appendChild(name);
+    additionalFields.appendChild(document.createElement('br'));
+    additionalFields.appendChild(label2);
+    additionalFields.appendChild(lastName);
+    additionalFields.appendChild(document.createElement('br'));
+    additionalFields.appendChild(label3);
+    additionalFields.appendChild(phone);
+    additionalFields.appendChild(document.createElement('br'));
+    additionalFields.appendChild(label4);
+    additionalFields.appendChild(email);
+    additionalFields.appendChild(document.createElement('br'));
 
     // Marcar que los campos ya han sido agregados
     fieldsAdded = true;
 }
-
 
 function registerClient() {
     var name = document.getElementById('name').value;
@@ -108,37 +114,47 @@ function registerClient() {
     var phone = document.getElementById('phone').value;
     var email = document.getElementById('email').value;
     var dni = document.getElementById('dni').value;
+    var dependencia = document.getElementById('dependencia').value;
 
-    $.ajax({
-        url: '../../../Backend/php/registers/registerClients.php',
-        method: 'POST',
-        data: {
-            dni: dni,
-            name: name,
-            lastName: lastName,
-            phone: phone,
-            email: email
-        },
-        success: function (response) {
-            console.log('Server response:', response); // Agregar esta línea para ver la respuesta del servidor
-            try {
-                var jsonResponse = JSON.parse(response);
-                if (jsonResponse.type === 1) {
-                    var modal = document.getElementById('turnoModal');
-                    modal.style.display = "block";
-                } else {
-                    alert('Hubo un error al registrar el cliente: ' + jsonResponse.message);
+    // Verificar que todos los campos están completos y que la dependencia es válida
+    if (name && lastName && phone && email && dni && dependencia !== '') {
+        $.ajax({
+            url: '../../../Backend/php/registers/registerClients.php',
+            method: 'POST',
+            data: {
+                dni: dni,
+                name: name,
+                lastName: lastName,
+                phone: phone,
+                email: email,
+                dependencia: dependencia
+            },
+            success: function (response) {
+                //línea para ver la respuesta del servidor
+                console.log('Server response:', response); 
+                try {
+                    var jsonResponse = JSON.parse(response);
+                    if (jsonResponse.type === 1) {
+                        var modal = document.getElementById('turnoModal');
+                        modal.style.display = "block";
+                    } else {
+                        alert('Hubo un error al registrar el cliente: ' + jsonResponse.message);
+                    }
+                } catch (e) {
+                    alert('Hubo un error al procesar la respuesta del servidor. Por favor, inténtelo nuevamente.');
+                    // línea para ver el error en la consola
+                    console.error('Error parsing JSON response:', e); 
                 }
-            } catch (e) {
-                alert('Hubo un error al procesar la respuesta del servidor. Por favor, inténtelo nuevamente.');
-                console.error('Error parsing JSON response:', e); // Agregar esta línea para ver el error en la consola
+            },
+            error: function (xhr, status, error) {
+                alert('Hubo un error con la solicitud AJAX. Por favor, inténtelo nuevamente.');
+                // línea para ver el error en la consola
+                console.error('AJAX error:', status, error); 
             }
-        },
-        error: function (xhr, status, error) {
-            alert('Hubo un error con la solicitud AJAX. Por favor, inténtelo nuevamente.');
-            console.error('AJAX error:', status, error); // Agregar esta línea para ver el error en la consola
-        }
-    });
+        });
+    } else {
+        alert('Por favor, complete todos los campos y seleccione una dependencia válida.');
+    }
 }
 
 // Obtener el elemento de cierre de la ventana modal
