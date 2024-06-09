7aab17ffb4d31a91fb7c019e9b7249a80e465f3f
Booh-rm <stivenatis@udenar.edu.co>
2024-06-08 21:29:42 -0500

    Creación de php para obtener informacion de un usuario

---
+++ Backend/php/gets/getUser.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+$user_id = $_SESSION['user_id'];
+$email = $_POST["email"];
+
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$database = "shift_management_system";
+
+$conn = mysqli_connect($serverName, $userName, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$response = array('type' => "success", 'nombre' => "-");
+
+$sql = "SELECT * FROM tb_funcionario WHERE email = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$row = $result->fetch_assoc();
+
+$response['nombre'] = $row['nombre'];
+
+echo json_encode($response);
+
+$conn->close();
+?>
