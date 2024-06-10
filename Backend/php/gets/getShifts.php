0b1b4e3b1a5ece75a146ae0655efb7a95761fb29
Booh-rm <stivenatis@udenar.edu.co>
2024-06-10 10:49:22 -0500

    Script para función de vista de turnos

---
+++ Backend/php/gets/getShifts.php
@@ -0,0 +1,36 @@
+<?php
+
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$database = "shift_management_system";
+
+$conn = mysqli_connect($serverName, $userName, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Consulta para obtener los turnos y las descripciones de las dependencias
+$sql = "SELECT t.id_turno, tc.descripcion 
+        FROM tb_turno t 
+        JOIN tb_tipo_consulta tc ON t.id_consulta = tc.codigo";
+
+$result = $conn->query($sql);
+
+// Array para almacenar los turnos
+$turnos = array();
+
+// Recorre los resultados de la consulta y añade los turnos al array
+while ($row = $result->fetch_assoc()) {
+    $turnos[] = [
+        'id_turno' => $row['id_turno'],
+        'descripcion' => $row['descripcion']
+    ];
+}
+
+$conn->close();
+
+echo json_encode($turnos);
+
+?>
